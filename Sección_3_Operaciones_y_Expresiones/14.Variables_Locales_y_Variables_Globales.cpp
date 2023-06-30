cd4aecd537b0da4c616bbb9711dc243fc2430d3b
JoshuaMendez <joshuamendez0501@gmail.com>
2023-06-29 20:07:52 -0500

    Sección 3

--- Sección_3_Operaciones_y_Expresiones/14.Variables_Locales_y_Variables_Globales.cpp
+++ Sección_3_Operaciones_y_Expresiones/14.Variables_Locales_y_Variables_Globales.cpp
@@ -2,11 +2,18 @@
 
 #include <stdio.h>
 
+int variable = 0;
+
 int main(){
 	
 // Variables Locales
 	for (int i = 0; i < 16; i++){
 		printf("\nValor de i: %i", i);
-		i += 3;
+		i += 3; // La variable i solo se declaró en el bucle for. Variable Local
 	}
+	
+// Variables Globales
+	variable +=5
+	printf("\nValor de variable:")
+
 }
